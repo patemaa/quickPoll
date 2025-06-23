51bc7ff033dc214b9657b8799a58bf0a20ae0892
Fatma R <anthoschichequs@gmail.com>
2025-06-23 11:20:01 +0300

    tr dil dosyalari guncellendi.

--- quick-poll/lang/tr/passwords.php
+++ quick-poll/lang/tr/passwords.php
@@ -9,13 +9,14 @@ return [
     |
     | The following language lines are the default lines which match reasons
     | that are given by the password broker for a password update attempt
-    | outcome such as failure due to an invalid password / reset token.
+    | has failed, such as for an invalid token or invalid new password.
     |
     */
 
-    'reset' => 'Şifreniz sıfırlandı.',
-    'sent' => 'Şifre sıfırlama bağlantısını e-posta ile gönderdik.',
-    'throttled' => 'Lütfen yeniden denemeden önce bekleyin.',
-    'token' => 'Bu şifre sıfırlama anahtarı geçersiz.',
-    'user' => 'Bu e-posta adresine sahip bir kullanıcı bulunamadı.',
+    'reset'     => 'Parolanız başarıyla sıfırlandı!',
+    'sent'      => 'Parola sıfırlama bileti e-posta ile gönderildi!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token'     => 'Parola sıfırlama bileti geçersiz.',
+    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+
 ];
